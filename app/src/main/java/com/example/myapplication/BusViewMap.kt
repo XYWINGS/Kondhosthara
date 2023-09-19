b7d0b86b4ee5664ea9fced34c13da4fe16be703d
XYWINGS <it21209352@my.sliit.lk>
2023-09-19 22:22:16 +0530

    many driver things

--- app/src/main/java/com/example/myapplication/BusViewMap.kt
+++ app/src/main/java/com/example/myapplication/BusViewMap.kt
@@ -1,14 +1,17 @@
 package com.example.myapplication
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -22,12 +25,21 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.Circle
 import com.google.android.gms.maps.model.CircleOptions
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
 
 private lateinit var fusedLocationClient : FusedLocationProviderClient
 private lateinit var locationsRequest: com.google.android.gms.location.LocationRequest
 private lateinit var radiusTextView : TextView
+private lateinit var busDataTextView : TextView
 private lateinit var sliderRadius : SeekBar
 private lateinit var currentUserLocation : LatLng
+private lateinit var logOutBtn : Button
+private var isMapReady = false
 
 
 class BusViewMap : AppCompatActivity(), OnMapReadyCallback {
@@ -49,11 +61,17 @@ class BusViewMap : AppCompatActivity(), OnMapReadyCallback {
 
         sliderRadius = findViewById(R.id.sliderRadius)
         radiusTextView = findViewById(R.id.textViewRadius)
+        logOutBtn = findViewById(R.id.viewBusLogout)
+        busDataTextView = findViewById(R.id.textViewBusData)
+
+
         val radiusValues = arrayOf(1.0, 5.0, 10.0, 25.0, 50.0)
         var selectedRadius =  radiusValues[2]
 
         sliderRadius.min = 1
         sliderRadius.max = radiusValues.size - 1
+
+
 
         sliderRadius.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
@@ -65,10 +83,45 @@ class BusViewMap : AppCompatActivity(), OnMapReadyCallback {
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
                 radiusTextView.text = "Search Radius: ${selectedRadius.toInt()} km"
-                circleHandler(selectedRadius)
+                if (isMapReady){
+                    circleHandler(selectedRadius)
+                    getBusDetails()
+                }
+
             }
         })
 
+        logOutBtn.setOnClickListener {
+            Firebase.auth.signOut()
+            Toast.makeText(this, "Logging Out", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            this.finish()
+        }
+    }
+
+    private fun getBusDetails() {
+        val busReference = FirebaseDatabase.getInstance().reference.child("Buses")
+        busReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (ownerSnapshot in dataSnapshot.children) {
+                    for (busSnapshot in ownerSnapshot.children) {
+                        val destinationLat = busSnapshot.child("endLocation").child("latLng").child("latitude").value
+                        val destinationLong = busSnapshot.child("endLocation").child("latLng").child("longitude").value
+                     //   Log.d("Debug","Destination is $destination --------------------------------------------------------------")
+                        if (destinationLat != null && destinationLong != null) {
+
+                            val destinationText = "Latitude: $destinationLat, Longitude: $destinationLong"
+                            busDataTextView.text = destinationText
+                        }
+                    }
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle any errors here
+            }
+        })
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -77,6 +130,7 @@ class BusViewMap : AppCompatActivity(), OnMapReadyCallback {
         val zoomLevel = 8.0f
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sriLankaLatLng, zoomLevel))
         addCurrentLocationMarker()
+        isMapReady = true
     }
 
     private fun checkPermission(): Boolean {
